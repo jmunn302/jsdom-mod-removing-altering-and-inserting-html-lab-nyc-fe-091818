93365ecba29be0260949a20e1f8d80c37f595185
Julie Munn <jmunn302@gmail.com>
2018-10-23 21:53:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let h1 = 'newHeader'
+const newHeader = h1;
+var element = document.createElement('h1#victory')
+document.body.appendChild('h1')
+h1.innerHTML = 'Julie is the Champion'
